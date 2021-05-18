7670baabcd54e88c68f67ed96005434f63532b37
Raghav Mangal <raghav.mangal@nagarro.com>
2021-05-18 15:14:29 +0530

    Playing with razor view, many functionalities

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -89,6 +89,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 db.Update(restaurant);
+                TempData["Message"] = "You have saved the restaurant successfully!";
                 return RedirectToAction("Details", new { id = restaurant.Id });
             }
             return View(restaurant);
